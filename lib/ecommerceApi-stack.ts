e66dcd8a487ee903606ec6e6e400f7f0f75b44c4
Marco Lima <maurelima@gmail.com>
2024-12-14 21:14:25 -0300

    feat: creates integration between api gateway and lambda function

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,25 @@
+import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJS.NodejsFunction;
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
+        super(scope, id, props);
+
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "EcommerceApi",
+        })
+
+        const productsFetchLambda = new apigateway.LambdaIntegration(props.productsFetchHandler)
+
+        // /products
+        const productsResource = api.root.addResource('products');
+        productsResource.addMethod('GET', productsFetchLambda);
+    }
+}
